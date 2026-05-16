14dee9493ee0a8b9b49652bd0a886540b47d7583
Shuvo dev nath <shuvodevnath0188@gmail.com>
2026-05-16 17:25:17 +0600

    ListView custome task_list

--- task_manager/my_todo_app/views.py
+++ task_manager/my_todo_app/views.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from my_todo_app.models import Task
@@ -8,4 +10,13 @@ from my_todo_app.models import Task
 
 class TaskListView(ListView):
     model = Task
+    task_list = Task.objects.all()
     template_name = 'task_list.html'
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['task_list'] = self.task_list
+        return context
+    
+
+    
